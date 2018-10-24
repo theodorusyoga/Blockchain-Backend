815e5fb5d718a23a8fdc85566c88e3ced7702624
Theodorus <theoyoga@outlook.com>
2018-10-25 00:35:23 +0700

    Added dotenv with env example for db config. Changed config.json to config.js

---
+++ tebex-server/server/config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "operatorsAliases": false,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
+  },
+  "test": {
+    "operatorsAliases": false,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
+  },
+  "production": {
+    "operatorsAliases": false,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
+  }
+};
